c68c2385325a65cf847df597d2b4c245f76e2f00
Markem <pierrysavio.pi@gmail.com>
2026-01-03 19:40:36 -0300

    feat: add class StringWriter

---
+++ src/util/StringWriter.java
@@ -0,0 +1,22 @@
+package util;
+
+public class StringWriter {
+
+	private int speed;
+	
+	public StringWriter (int speed) {
+		this.speed = speed;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	public void write() {
+		//keep coding here
+	}
+}
